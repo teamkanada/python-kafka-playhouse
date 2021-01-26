92f5d6305949948d9327158c1295ad720b3cf5d5
Oto Plahcinski <otoplahcinski@stockx.com>
2021-01-26 01:17:53 -0500

    Move secret to env

--- src/app/settings.py
+++ src/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3=rdtp0m27-s4@%4@li69@sk-sgpu5+2r+if21im%x(htd8m4$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
